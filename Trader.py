5f2a34ed6a14b5ded0f85f92fcfd19b9ab418c67
Alikjan <alikjan88@gmail.com>
2023-09-17 07:43:50 +0300

    added calcAccountMoney function

--- Trader.py
+++ Trader.py
@@ -4,14 +4,15 @@ import numpy as np
 class Trader():
   def __init__(self, stock, money, comis, printBool):
     self.stock =  stock
-    self.volume = 0
+    self.posVolume = 0
     self.printBool = printBool
     #self.Stock_Cash = []
-    self.my_money = money
+    self.my_money = money #мои деньги которые никуда не вложены
     self.table = pd.DataFrame({'Date':[0], 'Stock_Cash':[0], "My_money":[self.my_money], "Account_money":[self.my_money] })
     self.dater = []
-    self.money_of_stock = 0
-    self.start_money = 0
+    self.money_of_stock = 0 #стоимость вложений, используется только для записи в таблицу параметров счета
+    self.start_money = 0 #нужен для вычисления шорта
+    self.money
     self.state = ''
     self.comis = comis
     self.moexComis = 50
@@ -20,59 +21,60 @@ class Trader():
   def calcComis(self, prices, volumes):
     return prices*volumes/100*self.comis + self.moexComis
         
-  def buy(self, volume, price):
-    if self.volume < 0:
+  def buy(self, orderVolume, price):
+    if self.posVolume < 0:
       if self.printBool == True:
-        print("\nBuy with price,", price, "Short_Close_Sum", volume * price, "\n")
-      self.my_money = self.my_money + self.start_money + self.start_money - volume * price - (price*volume/100*self.comis + 50)
-    self.volume = self.volume + volume
-    #print("self.volume", self.volume)
+        print("\nBuy with price,", price, "Short_Close_Sum", orderVolume * price, "\n")# Здесь может и не закрыться шорт
+      self.my_money = self.my_money + self.start_money + self.start_money - orderVolume * price - self.calcComis(price,orderVolume)
+    self.posVolume = self.posVolume + orderVolume
+    #print("self.posVolume", self.posVolume)
     #print("buying+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    #self.Stock_Cash.append(self.volume * price)
-    if self.volume > 0:
-      #print("self.volume > 0")
+    #self.Stock_Cash.append(self.posVolume * price)
+    if self.posVolume > 0:
+      #print("self.posVolume > 0")
       #print("self.printBool == True", self.printBool == True)
       if self.printBool == True:
-        print("\nBuy with price", price, "Open_Long_Sum", volume * price, "\n" )
+        print("\nBuy with price", price, "Open_Long_Sum", orderVolume * price, "\n" )
       self.state = 'Long'
-      self.my_money = self.my_money - volume * price  -  (price*volume/100*self.comis + 50)
-    elif self.volume == 0:
+      self.my_money = self.my_money - orderVolume * price  -  self.calcComis(price,orderVolume)
+    elif self.posVolume == 0:
       self.state = ''
       self.start_money = 0
       #print("stat0000000000000000000000000")
         
-  def sell(self, volume, price):
-    if self.volume > 0:
+  def sell(self, orderVolume, price):
+    if self.posVolume > 0:
       if self.printBool == True:
-        print("\nSell with price,", price, "Long_Close_Sum", volume * price, "\n")
-      self.my_money = self.my_money + volume * price -  (price*volume/100*self.comis + 50)
-      self.volume = self.volume - volume
-    elif self.volume == 0:
-      self.volume = self.volume - volume
-      self.my_money = self.my_money - volume * price -  (price*volume/100*self.comis + 50)
+        print("\nSell with price,", price, "Long_Close_Sum", orderVolume * price, "\n")
+      self.my_money = self.my_money + orderVolume * price -  self.calcComis(price,orderVolume)
+      self.posVolume = self.posVolume - orderVolume
+    elif self.posVolume == 0:
+      self.posVolume = self.posVolume - orderVolume
+      self.my_money = self.my_money - orderVolume * price -  self.calcComis(price,orderVolume)
         
-    if self.volume < 0:
+    if self.posVolume < 0:
       if self.printBool == True:
-        print("\nSell with price,", price, "Open_Short_sum", volume * price, "\n")
+        print("\nSell with price,", price, "Open_Short_sum", orderVolume * price, "\n")
       self.state = 'Short'
-      self.start_money = abs(volume * price)
+      self.start_money = abs(orderVolume * price)
       if self.printBool == True:
         print("SELL, self.start_money", self.start_money, "my_money", self.my_money, "\n")
-    elif self.volume == 0:
+    elif self.posVolume == 0:
       self.state = ''
       self.start_money = 0
+  
+  def calcAccountMoney(self, price):
+    if self.state == 'Short':
+      return self.start_money + self.start_money + self.posVolume * price + self.my_money
+    else:
+      return self.posVolume * price + self.my_money
 
   def quant_money(self, price, date_now):
-    self.money_of_stock =abs(self.volume * price)
-    #self.Stock_Cash.append(self.volume * price)
-    if self.state == 'Short':#325 + 325 - 314 + 15000
-      self.account_money =   self.start_money + self.start_money + self.volume * price + self.my_money
-      if self.printBool == True:
-        print("Quant money, self.account_money", self.account_money, "self.volume*price",  self.volume*price, "my_money, ", self.my_money, "cur_price, ", price, "\n")
-    else:
-      self.account_money = self.volume * price + self.my_money
-      if self.printBool == True:
-        print("Quant money, self.account_money", self.account_money, "self.volume*price",  self.volume*price, "my_money, ", self.my_money, "cur_price, ", price, "\n")
+    self.money_of_stock =abs(self.posVolume * price)
+    #self.Stock_Cash.append(self.posVolume * price)
+    self.account_money =  self.calcAccountMoney(price) #мои деньги и вложения
+    if self.printBool == True:
+      print("Quant money: Account_money", self.account_money, "posVolume*price",  self.posVolume*price, "my_money, ", self.my_money, "cur_price, ", price, "\n")
     df = pd.DataFrame({"Date":[date_now],
                       "Stock_Cash":[self.money_of_stock],
                       "My_money":[self.my_money],
